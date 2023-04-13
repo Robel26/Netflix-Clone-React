a7f6ff289477eaaa2dc65e4c48edba29bfb0271c
RobelAlemayehu <robinakassahun@gmail.com>
2023-04-13 10:01:04 -0500

    First commit

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "e88f607648d3710afa5dbc30bef2ec1f";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
